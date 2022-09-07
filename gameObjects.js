c36abbe4e2a3d2e9c6a686a05691362c5477b542
BFSMx1 <90730982+alberto-iram@users.noreply.github.com>
2022-09-07 09:47:54 -0500

    Update gameObjects.js

--- gameObjects.js
+++ gameObjects.js
@@ -1,33 +1,33 @@
 AFRAME.registerComponent("wire-fence", {
     init: function () {
-      //starting x position
+      //Posición inicial x
       posX = -20;
-      //starting z-position
+      //Posición inicial z
       posZ = 85;
   
       for (var i = 0; i < 10; i++) {
-        //create wire-fence entity
+        //Crear una entidad wire-fence
         var wireFence1 = document.createElement("a-entity");
         
   
-        //set x, y and z position
+        //Establecer las posiciones x, y, z
         posX = posX + 5;
         posY = 2.5;
         posZ = posZ - 10;
   
-        //scale 
+        //Escala 
         var scale = { x: 2, y: 2, z: 2 };
   
-        //set the id
+        //Establecer el ID
         wireFence1.setAttribute("id", "wireFence1" + i);
         
-        //set the position
+        //Establecer la posición
         wireFence1.setAttribute("position", { x: posX, y: 2.5, z: -35 });
         
-        //set the scale
+        //Establecer la escala
         wireFence1.setAttribute("scale", scale);
        
-        //set the model
+        //Establecer el modelo
         wireFence1.setAttribute(
           "gltf-model",
           "./models/barbed_wire_fence/scene.gltf"
@@ -35,11 +35,11 @@ AFRAME.registerComponent("wire-fence", {
   
   
 
-        //set the physics body
+        //Establecer el cuerpo físico
         wireFence1.setAttribute("static-body", {});
        
         var sceneEl = document.querySelector("#scene");
-        //attach the entity to the scene
+        //Añadir la entidad a la escena
         sceneEl.appendChild(wireFence1);
         
   
